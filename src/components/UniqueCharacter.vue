a4784875adabc458dfaf2057efc0e66de41c2e8c
Michellemukami <kamimello463@gmail.com>
2022-08-17 10:32:26 +0300

    fixing the string

--- src/components/UniqueCharacter.vue
+++ src/components/UniqueCharacter.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="top" class="bg-cover bg-no-repeat bg-center flex items-center justify-center h-screen">
 <div class="w-full max-w-xs">
-  <div v-if="alertOpen">
-  <div class="pb-4" v-if="errors.length">
-  <div class="flex bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-  <div class="py-1"><svg class="fill-current h-5 w-5 text-read-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
-  <ul>
-      <li v-for="error in errors" :key="error">{{ error }}</li>
-    </ul>
-  <span v-on:click="closeAlert()" class="absolute top-0 bottom-0 right-0 px-4 py-3">
-    <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
-  </span>
-</div>
-</div>
+  <transition-group name="block">
+    <div v-if="alertOpen">
+    <div class="pb-4" v-if="errors.length">
+    <div class="flex bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+    <div class="py-1"><svg class="fill-current h-5 w-5 text-read-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+    <ul>
+        <li v-for="error in errors" :key="error">{{ error }}</li>
+      </ul>
+    <span v-on:click="closeAlert()" class="absolute top-0 bottom-0 right-0 px-4 py-3">
+      
+      <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+    </span>
   </div>
+  </div>
+    </div>
+  </transition-group>
 
   <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
     <div class="mb-4">
@@ -31,7 +34,7 @@
           Clear
         </button> -->
         <div v-if="imgShow">
-          <iframe  id="giff" src="https://giphy.com/embed/Bve8CDLZCFuZa" width="100" height="100" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/stickers/pikachu-Bve8CDLZCFuZa"></a></p>
+          <iframe  id="giff" src="https://giphy.com/embed/Bve8CDLZCFuZa" width="100" height="100" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>
           <p>Processing...</p>
         </div>
      
@@ -59,8 +62,10 @@ export default {
     }
   },
   methods:{
-      Clean(){
+    Clean(){
     document.getElementById('display').innerHTML='';
+    
+    
     },
     closeAlert: function(){
       this.alertOpen = false;
@@ -75,6 +80,7 @@ export default {
       this.imgShow = true;
       this.buttonShow = false
     }
+  
   }
   this.errors = [];
   if (this.data) {
@@ -94,8 +100,6 @@ export default {
   }
  
 }
-
-
 </script>
 <style>
 #top {
@@ -104,6 +108,37 @@ export default {
     background-repeat: no-repeat;
     background-size: cover;
 }
+ .block-enter {
+      opacity: 0;
+    }
+    .block-enter-active {
+      animation : slide-in 0.5s ease-out forwards;
+      transition : opacity 0.5s;
+    }
+    .block-leave {
+        opacity: 1;
+    }
+    .block-leave-active {
+       animation : slide-out 0.5s ease-out forwards;
+       transition : opacity 1s;
+       opacity: 0;
+    }
+    @keyframes slide-in {
+      from {
+         transform: translateY(20px);
+      }
+      to {
+         transform: translateY(0);
+      }
+    }
+    @keyframes slide-out {
+      from {
+        transform: translateY(0);
+      }
+      to {
+         transform: translateY(-20px);
+      }
+    }
 </style>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 
